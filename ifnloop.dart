8cf30d71427bc7304565615f6aeee63b9f9788c3
ervindominikus <ervindominikus@gmail.com>
2026-09-10 20:17:26 +0700

    Versi 2

---
+++ ifnloop.dart
@@ -0,0 +1,91 @@
+void main {
+
+//     if
+
+//     int nilai = 85;
+// if (nilai > 85) {
+//     print('nilai di atas 85');
+// } else {
+//     if (nilai > 85)
+//     print('nilai di bawah 85');
+// }    
+
+// if (nilai > 85 ){
+//     print('nilai di atas 85');
+// }
+// if(nilai < 85) {
+//     print('nilai di bawah 85');
+// }
+// switch
+// var nilai = 'A';
+// switch (nilai) {
+//     case 'A':
+//     print('nilai di atas adalah A');
+//     break;
+//     case 'B':
+//     print('nilai di atas bawah C');
+//     case 'C':
+//     print('nilai di atas bawah C');
+//     break;
+//     default:
+//     print('nilai diluar pilihan');
+// for
+// for in
+// foreach
+// while
+// dowhile
+
+// for(var i = 0; i < 10; i++) {
+//     print('Nilai $i');
+// }
+
+// List datamhs = ['Deni,' 'Windy', 'Sindy','Micheal', 'Jonathan'];
+// for (var i =0; i < datamhs.length; i++) {
+//     print('nama mhs = ${datamhs[i]}');
+// }
+
+// for in
+// List datamhs = ['Deni,' 'Windy', 'Sindy','Micheal', 'Jonathan'];
+
+// for (var element in datamhs) {
+//     if (element == 'Deni') {
+//     print('ini deni');
+// }
+// print (element);
+// }
+// datamhs.foreach((e)) {
+//     // print(e);
+//     if (a == 'Deni') {
+//         print(a);
+// }
+// }}
+
+// while
+// var nilai =10;
+//  while(nilai < 5) {
+//     print(nilai);
+// nilai++;
+//  }
+
+// List datamhs = ['Deni,' 'Windy', 'Sindy','Micheal', 'Jonathan'];
+// var index=0;
+// while (index < datamhs.length) {
+//     print(datamhs[index]);
+//     index++;
+// }
+
+// do wihile
+//  List datamhs = ['Deni,' 'Windy', 'Sindy','Micheal', 'Jonathan'];
+//  var index=0;
+//  do {
+//  print(datamhs)
+//  index++;
+// } while (index < datamhs.length);
+
+stdout.writeln('Masukan Angka 1:');
+var inputUser1 = stdin.readlineSync()!;
+stdout.writeln('Masukan Angka 2:');
+var inputUser2 = stdin.readlineSync()!;
+
+num hasil = int.parse(inputUser1) + 
+ }
